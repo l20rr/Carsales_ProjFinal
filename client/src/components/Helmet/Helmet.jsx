1432f7a9d0a853619f16968ef597761d7c2b1c3c
23Faria <pedro.faria011@gmail.com>
2023-04-14 16:42:28 +0100

    Titulo do projeto

--- client/src/components/Helmet/Helmet.jsx
+++ client/src/components/Helmet/Helmet.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Helmet = (props) => {
-  document.title = "Rent Car Service - " + props.title;
+  document.title = "Auto Classificados - " + props.title;
   return <div className="w-100">{props.children}</div>;
 };
 
