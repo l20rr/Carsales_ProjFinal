bfc80b124b24874cb884267ad9ebcc606d8eb54e
Daniel Ferreira <daniel_carra@hotmail.com>
2023-04-14 16:20:42 +0100

    Add dropdown profile options

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -10,6 +10,10 @@ import "../../styles/header.css";
 import Cookies from 'universal-cookie';
 import { StreamChat } from 'stream-chat';
 
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Button from 'react-bootstrap/Button';
+
 const navLinks = [
   {
     path: "/home",
@@ -52,12 +56,14 @@ const Header = () => {
   const toggleMenu = () => menuRef.current.classList.toggle("menu__active");
 
   return (
+    
     <header className="header">
       
       {/* ========== main navigation =========== */}
 
       <div className="main__navbar">
         <Container>
+          
           <div className="navigation__wrapper d-flex align-items-center justify-content-between">
             <span className="mobile__menu">
               <i class="ri-menu-line" onClick={toggleMenu}></i>
@@ -90,23 +96,34 @@ const Header = () => {
             
             <Col lg="6" md="6" sm="6">
               <div className="header__top__right d-flex align-items-center justify-content-end gap-3">
-
+              
               {
               authToken ?
               
-               
-                 <button className=" d-flex align-items-center gap-1" onClick={logout}>
-                  <i class="ri-user-line"></i>Logout 
-                </button>
-             
-              
+                <Dropdown>
+                  <Dropdown.Toggle variant="outline-secondary" id="dropdown-basic" size="sm" idx="sm">
+                    <i class="ri-user-line"></i> A tua conta 
+                  </Dropdown.Toggle>
+
+                <Dropdown.Menu variant="dark" >
+                  <Dropdown.Header>Alberto</Dropdown.Header>
+                  <Dropdown.Header>A tua conta</Dropdown.Header>
+                  <Dropdown.Item href="#/action-1">Perfil</Dropdown.Item>
+                  <Dropdown.Item href="#/action-2">Mensagens</Dropdown.Item>
+                  <Dropdown.Item href="#/action-3">Favoritos</Dropdown.Item>
+                  <Dropdown.Divider />
+                  <Dropdown.Item onClick={logout}>Sair</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+      
               :
-              
-                <Link to="register" className=" d-flex align-items-center gap-1" >
-                  <i class="ri-user-line" ></i> Entrar/Registar
-                </Link>
-              
+
+              <Link to="register" className=" d-flex align-items-center gap-1" >
+                <Button variant="outline-secondary" size="sm"> <i class="ri-user-line"></i> A tua conta</Button>
+              </Link>
+
               }  
+              
               </div>
             </Col>
           </div>
