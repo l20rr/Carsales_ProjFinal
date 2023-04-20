7f69ec20d36605c1a3cfadeaa149d7cff7b81d7d
23Faria <pedro.faria011@gmail.com>
2023-04-20 11:40:00 +0100

    Caixa Filtros na Página "Cars"

--- client/src/pages/CarListing.jsx
+++ client/src/pages/CarListing.jsx
@@ -5,12 +5,29 @@ import CommonSection from "../components/UI/CommonSection";
 import CarItem from "../components/UI/CarItem";
 import carData from "../assets/data/carData";
 import { Link } from "react-router-dom";
+import HeroSlider from "../components/UI/HeroSlider";
+import FindCarForm from "../components/UI/FindCarForm";
 
 const CarListing = () => {
   return (
     <Helmet title="Cars">
+      
       <CommonSection title="veículo" />
+      <section className="p-3 hero__slider-section">
+        <div className="hero__form">
+            <Container>
+              <Row className="form__row">
+                <Col lg="1" md="1">
+                  
+                </Col>
 
+                <Col lg="10" md="10" sm="12">
+                  <FindCarForm />
+                </Col>
+              </Row>
+            </Container>
+          </div>
+        </section>
       <section>
         <Container>
           <Row>
