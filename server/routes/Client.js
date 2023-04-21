0ac3854a385cf7be5d85c109554545d7dfa621a6
23Faria <pedro.faria011@gmail.com>
2023-04-21 11:52:20 +0100

    Atualizaçao Server/routes

--- server/routes/Client.js
+++ server/routes/Client.js
@@ -3,60 +3,81 @@ const router = express.Router();
 const db = require('../models');
 const Client = db.client;
 
-router.post("/userData", async (req, res) => {
-    const {locality, telem, birthdate, userId} = req.body;
- 
+router.post("/userData", async(req, res) => {
+    const { locality, telem, birthdate, userId } = req.body;
+
     try {
-          console.log(req.body);
-         await Client.create({
+        console.log(req.body);
+        await Client.create({
             locality: locality,
             telem: telem,
             birthdate: birthdate,
             userId: userId
         });
 
-        res.status(201).json({msg: "Register Berhasil"});
+        res.status(201).json({ msg: "Register Berhasil" });
     } catch (error) {
-        res.status(400).json({msg: error.message});
+        res.status(400).json({ msg: error.message });
     }
- 
+
 });
 
-
-router.get("/All", async (req, res) => {
-  try {
-      const response = await Client.findAll({
-          attributes:['userId',]
-      });
-      res.status(200).json(response);
-  } catch (error) {
-      res.status(500).json({msg: error.message});
-  }
-});
-
-
-router.put("/:id", async (req, res) => {
+router.get("/:id", async(req, res) => {
     const id = req.params.id;
-  
+
+    Client.findByPk(id)
+        .then(data => {
+            if (data) {
+                res.send(data);
+            } else {
+                console.log("error")
+                res.status(404).send({
+                    message: `Cannot find with id=${id}.`
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Error retrieving  with id=" + id
+            });
+        });
+});
+
+
+router.get("/All", async(req, res) => {
+    try {
+        const response = await Client.findAll({
+            attributes: ['ID', 'userId', 'locality', 'telem', 'birthdate']
+        });
+        res.status(200).json(response);
+    } catch (error) {
+        res.status(500).json({ msg: error.message });
+    }
+});
+
+
+router.put("/:id", async(req, res) => {
+    const id = req.params.id;
+
     try {
         const num = await Client.update(req.body, {
-          where: { userId: id },
+            where: { userId: id },
         });
-    
-        if (num == 1) {
-          res.send({
-            message: "Client was updated successfully.",
-          });
-        } else {
-          res.send({
-            message: `Cannot update Client with id=${id}. Maybe Client was not found or req.body is empty!`,
-          });
-        }
-      } catch (err) {
-        res.status(500).send({
-          message: "Error updating Client with id=" + id,
-        });
-      }
-    });
 
-    module.exports = router;
+        if (num == 1) {
+            res.send({
+                message: "Client was updated successfully.",
+            });
+        } else {
+            res.send({
+                message: `Cannot update Client with id=${id}. Maybe Client was not found or req.body is empty!`,
+            });
+        }
+    } catch (err) {
+        res.status(500).send({
+            message: "Error updating Client with id=" + id,
+        });
+    }
+});
+
+module.exports = router;
--- server/routes/Users.js
+++ server/routes/Users.js
@@ -16,95 +16,96 @@ const api_key = process.env.STREAM_API_KEY;
 const api_secret = process.env.STREAM_API_SECRET;
 const app_id = process.env.STREAM_APP_ID;
 
-router.post('/signup', async (req, res) => {
+router.post('/signup', async(req, res) => {
     try {
         const { fullname, email, password } = req.body;
-    
-        
+
+
 
         const userId = crypto.randomBytes(16).toString('hex');
-    
+
         const serverClient = connect(api_key, api_secret, app_id);
 
         const hashedPassword = await bcrypt.hash(password, 10);
 
         const token = serverClient.createUserToken(userId);
-    
+
         // criando o usuário
         Users.create({
-          fullname: fullname,
-          email: email,
-          password: hashedPassword
+            fullname: fullname,
+            email: email,
+            password: hashedPassword
         });
 
-     
-    
+
+
         res.status(200).json({ token, userId, fullname, email, hashedPassword });
-      } catch (error) {
+    } catch (error) {
         console.log(error);
         res.status(500).json({ message: error });
-      }
-    });
+    }
+});
 
-router.post('/login', async (req, res) => {
+router.post('/login', async(req, res) => {
     try {
         const { email, password } = req.body;
-        
+
         const serverClient = connect(api_key, api_secret, app_id);
         const client = StreamChat.getInstance(api_key, api_secret);
 
         const { users } = await client.queryUsers({ name: email });
 
-        if(!users.length) return res.status(400).json({ message: 'User not found' });
+        if (!users.length) return res.status(400).json({ message: 'User not found' });
 
         const success = await bcrypt.compare(password, users[0].hashedPassword);
 
         const token = serverClient.createUserToken(users[0].id);
 
-        if(success) {
-            res.status(200).json({ token, fullname: users[0].fullname, email, userId: users[0].id});
+        if (success) {
+            res.status(200).json({ token, fullname: users[0].fullname, email, userId: users[0].id });
         } else {
             res.status(500).json({ message: 'Incorrect password' });
         }
-    } catch (error) {ads
+    } catch (error) {
+
         console.log(error);
 
         res.status(500).json({ message: error });
     }
 });
 
-router.get("/AllUsers", async (req, res) => {
+router.get("/AllUsers", async(req, res) => {
     try {
         const response = await Users.findAll({
-            attributes:['id',]
+            attributes: ['id', 'fullname', 'email']
         });
         res.status(200).json(response);
     } catch (error) {
-        res.status(500).json({msg: error.message});
+        res.status(500).json({ msg: error.message });
     }
-  });
+});
 
-router.get("/Users/:id", async (req, res) => {
+router.get("/Users/:id", async(req, res) => {
     const id = req.params.id;
     Users.findByPk(id)
-      .then(data => {
-        if (data) {
-          res.send(data);
-        } else {
-            console.log("error")
-          res.status(404).send({
-            message: `Cannot find User with id=${id}.`
-          });
-        }
-      })
-      .catch(err => {
-        res.status(500).send({
-          message: "Error retrieving User with id=" + id
+        .then(data => {
+            if (data) {
+                res.send(data);
+            } else {
+                console.log("error")
+                res.status(404).send({
+                    message: `Cannot find User with id=${id}.`
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Error retrieving User with id=" + id
+            });
         });
-      });
-  });
+});
 
 
-  
-  
+
+
 module.exports = router;
