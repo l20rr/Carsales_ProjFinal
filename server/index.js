f63afe2423481f098d62af6c2cfb8ef35646e4bb
23Faria <pedro.faria011@gmail.com>
2023-04-30 11:35:29 +0100

    Querie Favorites

--- server/index.js
+++ server/index.js
@@ -31,143 +31,148 @@ app.use(express.urlencoded({ extended: true }));
 
 
 //connect without erasing data!!!!!!!!!!
+db.sequelize.sync({})
+    .then(() => {
+        console.log("Synced db.");
 
-        
-db.sequelize.sync({ }).then(() => {
-    console.log("Drop and re-sync db.");
+        //connect by erasing data
 
-})
-        // create the admin, if they do not already exist
-        const password="1234";
-        bcrypt.hash(password, 10).then((hash)=>{
-            User.create({
-                fullname: "admin",
-                email: 'admin@gmail.com',
-                password: hash,
-                name: "admin",
-                admin: true,
+        /*
+        db.sequelize.sync({ force: true }).then(() => {
+            console.log("Drop and re-sync db.");
+
+
+            // create the admin, if they do not already exist
+            const password = "1234";
+            bcrypt.hash(password, 10).then((hash) => {
+                User.create({
+                    fullname: "admin",
+                    email: 'admin@gmail.com',
+                    password: hash,
+                    name: "admin",
+                    admin: true,
+                }).catch(err => {
+                    console.log("Already exists");
+                });
+            });
+
+            Category.create({
+                categoryName: "Carro"
             }).catch(err => {
                 console.log("Already exists");
+                console.log(err)
             });
-        });
-        
-        Category.create({
-            categoryName: "Carro"
-        }).catch(err => {
-            console.log("Already exists");
-            console.log(err)
-        });
-        Category.create({
-            categoryName: "Autocaravana"
-        }).catch(err => {
-            console.log("Already exists");
-            console.log(err)
-        });
-        Category.create({
-            categoryName: "Moto"
-        }).catch(err => {
-            console.log("Already exists");
-            console.log(err)
-        });
-        subcategory.create({
-            categoryID: 1,
-            SubcategoryName: "SUV"
-        }).catch(err => {
-            console.log("Already exists");
-            console.log(err)
-        });
-        subcategory.create({
-            categoryID: 1,
-            SubcategoryName: "City"
-        }).catch(err => {
-            console.log("Already exists");
-            console.log(err)
-        });
-        subcategory.create({
-            categoryID: 1,
-            SubcategoryName: "Sedan"
-        }).catch(err => {
-            console.log("Already exists");
-            console.log(err)
-        });
-        subcategory.create({
-            categoryID: 1,
-            SubcategoryName: "Desportivo"
-        }).catch(err => {
-            console.log("Already exists");
-            console.log(err)
-        });
-        subcategory.create({
-            categoryID: 1,
-            SubcategoryName: "4x4"
-        }).catch(err => {
-            console.log("Already exists");
-            console.log(err)
-        });
-        subcategory.create({
-            categoryID: 2,
-            SubcategoryName: "Campervan"
-        }).catch(err => {
-            console.log("Already exists");
-            console.log(err)
-        });
-        subcategory.create({
-            categoryID: 2,
-            SubcategoryName: "Capucino"
-        }).catch(err => {
-            console.log("Already exists");
-            console.log(err)
-        });
-        subcategory.create({
-            categoryID: 2,
-            SubcategoryName: "Perfilada"
-        }).catch(err => {
-            console.log("Already exists");
-            console.log(err)
-        });
-        subcategory.create({
-            categoryID: 2,
-            SubcategoryName: "Integral"
-        }).catch(err => {
-            console.log("Already exists");
-            console.log(err)
-        });
-        subcategory.create({
-            categoryID: 3,
-            SubcategoryName: "Street"
-        }).catch(err => {
-            console.log("Already exists");
-            console.log(err)
-        });
-        subcategory.create({
-            categoryID: 3,
-            SubcategoryName: "Custom"
-        }).catch(err => {
-            console.log("Already exists");
-            console.log(err)
-        });
-        subcategory.create({
-            categoryID: 3,
-            SubcategoryName: "Naked"
-        }).catch(err => {
-            console.log("Already exists");
-            console.log(err)
-        });
-        subcategory.create({
-            categoryID: 3,
-            SubcategoryName: "Scooter"
-        }).catch(err => {
-            console.log("Already exists");
-            console.log(err)
-        });
-        subcategory.create({
-            categoryID: 3,
-            SubcategoryName: "Sport"
-        }).catch(err => {
-            console.log("Already exists");
-            console.log(err)
-        });
-    
+            Category.create({
+                categoryName: "Autocaravana"
+            }).catch(err => {
+                console.log("Already exists");
+                console.log(err)
+            });
+            Category.create({
+                categoryName: "Moto"
+            }).catch(err => {
+                console.log("Already exists");
+                console.log(err)
+            });
+            subcategory.create({
+                categoryID: 1,
+                SubcategoryName: "SUV"
+            }).catch(err => {
+                console.log("Already exists");
+                console.log(err)
+            });
+            subcategory.create({
+                categoryID: 1,
+                SubcategoryName: "City"
+            }).catch(err => {
+                console.log("Already exists");
+                console.log(err)
+            });
+            subcategory.create({
+                categoryID: 1,
+                SubcategoryName: "Sedan"
+            }).catch(err => {
+                console.log("Already exists");
+                console.log(err)
+            });
+            subcategory.create({
+                categoryID: 1,
+                SubcategoryName: "Desportivo"
+            }).catch(err => {
+                console.log("Already exists");
+                console.log(err)
+            });
+            subcategory.create({
+                categoryID: 1,
+                SubcategoryName: "4x4"
+            }).catch(err => {
+                console.log("Already exists");
+                console.log(err)
+            });
+            subcategory.create({
+                categoryID: 2,
+                SubcategoryName: "Campervan"
+            }).catch(err => {
+                console.log("Already exists");
+                console.log(err)
+            });
+            subcategory.create({
+                categoryID: 2,
+                SubcategoryName: "Capucino"
+            }).catch(err => {
+                console.log("Already exists");
+                console.log(err)
+            });
+            subcategory.create({
+                categoryID: 2,
+                SubcategoryName: "Perfilada"
+            }).catch(err => {
+                console.log("Already exists");
+                console.log(err)
+            });
+            subcategory.create({
+                categoryID: 2,
+                SubcategoryName: "Integral"
+            }).catch(err => {
+                console.log("Already exists");
+                console.log(err)
+            });
+            subcategory.create({
+                categoryID: 3,
+                SubcategoryName: "Street"
+            }).catch(err => {
+                console.log("Already exists");
+                console.log(err)
+            });
+            subcategory.create({
+                categoryID: 3,
+                SubcategoryName: "Custom"
+            }).catch(err => {
+                console.log("Already exists");
+                console.log(err)
+            });
+            subcategory.create({
+                categoryID: 3,
+                SubcategoryName: "Naked"
+            }).catch(err => {
+                console.log("Already exists");
+                console.log(err)
+            });
+            subcategory.create({
+                categoryID: 3,
+                SubcategoryName: "Scooter"
+            }).catch(err => {
+                console.log("Already exists");
+                console.log(err)
+            });
+            subcategory.create({
+                categoryID: 3,
+                SubcategoryName: "Sport"
+            }).catch(err => {
+                console.log("Already exists");
+                console.log(err)
+            });*/
+    });
 
 
 
